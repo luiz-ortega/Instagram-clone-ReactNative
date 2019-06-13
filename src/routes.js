798fe081ac68b96f0ad9cf3cf3762920e2058d77
luiz-ortega <luiz@luby.com.br>
2019-06-13 19:17:07 -0300

    Routes and Pages

---
+++ src/routes.js
@@ -0,0 +1,13 @@
+import { createAppContainer, createStackNavigator } from 'react-navigation';
+
+import Feed from './pages/Feed';
+import New from './pages/New';
+
+export default createAppContainer(
+    createStackNavigator({
+        Feed,
+        New,
+      
+     
+    })
+);
